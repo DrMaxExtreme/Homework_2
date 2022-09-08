3cd67ff01f0de2c0b8587c0014562f0e13958c46
DrMax <tehnolog3ooo@yandex.ru>
2022-09-08 16:47:58 +0300

    HW_2_02

--- Assets/Scripts/SpawnersEnemy.cs
+++ Assets/Scripts/SpawnersEnemy.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnersEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform _enemySpawner;
+    [SerializeField] private int _seconsdPerSpawn;
+
+    public GameObject Enemy;
+
+    private Transform[] _spawners;
+    private int _currentSpawner;
+
     void Start()
     {
-        
+        _spawners = new Transform[_enemySpawner.childCount];
+
+        for(int i = 0; i < _enemySpawner.childCount; i++)
+        {
+            _spawners[i] = _enemySpawner.GetChild(i);
+        }
+
+        StartCoroutine(Spawning());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Spawning()
     {
-        
+        while (true)
+        {
+            Instantiate(Enemy, _spawners[Random.Range(Math.Round(_enemySpawner.childCount))]);//?
+
+            yield return new WaitForSeconds(_seconsdPerSpawn);
+        }
     }
 }
